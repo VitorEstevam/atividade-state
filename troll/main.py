a8de101c421a07adff12661a552a7127578affd3
Vitor Estevam <vitorestevam02@gmail.com>
2021-06-02 17:51:29 -0300

    format

--- troll/main.py
+++ troll/main.py
@@ -5,30 +5,35 @@ import random
 class Troll(Agent):
     life = 10
     hungry = 0
+
     def __init__(self):
         super().__init__(TrollStateSleeping())
         pass
+
     def update(self):
-        if(self.life <=0): self.set_state(TrollStateDead())
-        if(self.hungry >4): self.set_state(TrollStateHunting())
-        
+        if(self.life <= 0):
+            self.set_state(TrollStateDead())
+        if(self.hungry > 4):
+            self.set_state(TrollStateHunting())
         super().update()
 
 
 class TrollStateSleeping(State):
     def execute(self, troll: Troll):
         print("Mike is sleeping")
-        if(troll.life<10): troll.life+=1
-        troll.hungry+=1
+        if(troll.life < 10):
+            troll.life += 1
+        troll.hungry += 1
 
         _input = input("Do you want fight some noobs?(y/n)\n")
-        if(_input == "y"): 
+        if(_input == "y"):
             print("Mike is going to fight.")
             troll.set_state(TrollStateFighting())
-    
+
+
 class TrollStateFighting(State):
     def execute(self, troll: Troll):
-        damage = random.randint(2,4)
+        damage = random.randint(2, 4)
         troll.life -= damage
         print(f"After the fight Mike has taken {damage} from damage")
         troll.set_state(TrollStateSleeping())
@@ -37,11 +42,14 @@ class TrollStateFighting(State):
 class TrollStateHunting(State):
     def execute(self, troll: Troll):
         print("Mike is hunting")
-        troll.hungry-=3
-        if(troll.hungry<0): troll.hungry = 0
-        troll.life-=1
+        troll.hungry -= 3
+        if(troll.hungry < 0):
+            troll.hungry = 0
+        troll.life -= 1
 
-        if(troll.hungry == 0): troll.set_state(TrollStateSleeping())
+        if(troll.hungry == 0):
+            troll.set_state(TrollStateSleeping())
+
 
 class TrollStateDead(State):
     def execute(self, troll: Troll):
